b052f8173206ae71193b6b9a3ce6dc0c226e83c5
Bulle <bulle.mostovoi@gmail.com>
2025-11-12 15:19:45 +0100

    changed collisions a bit

--- scripts/main_cam.gd
+++ scripts/main_cam.gd
@@ -11,10 +11,40 @@ var rng = RandomNumberGenerator.new()
 var curr_shake_strength: float = 0.0
 @onready var starting_pos = position
 
+var position_tween: Tween
+var zoom_tween: Tween
+@onready var initial_zoom = zoom
+
 func start_shake(strength = 2.0, fade = 5.0) -> void:
 	curr_shake_strength = strength
 	shake_fade = fade
 
+func get_target_pos():
+	var snake_head_pos = %SnakeLayer.map_to_local(%SnakeManager.body[0])
+	var anchor = %SnakeLayer.map_to_local(%SnakeManager.body[0] + Direction.dir_to_vec(%SnakeManager.curr_dir)) # One tile after head
+	var dir_scaled = anchor - snake_head_pos # Vector of norm 1 in the right direction (conversion from tiles to local coordinates)
+
+	var head_offset = (dir_scaled * %SnakeManager.clock) / 16 # where is the head inside its tile, prevents jittering
+	var exact_head_pos =  %SnakeLayer.map_to_local(%SnakeManager.body[0]) + head_offset
+	var diff_vector = exact_head_pos - starting_pos
+	return starting_pos + diff_vector * max(0, diff_vector.length() - 10)* .001
+
+func on_collision():
+	var snake_head_pos = %SnakeLayer.map_to_local(%SnakeManager.body[0])
+	var anchor = %SnakeLayer.map_to_local(%SnakeManager.body[0] + Direction.dir_to_vec(%SnakeManager.curr_dir)) # One tile after head
+	var dir_scaled = anchor - snake_head_pos # Vector of norm 1 in the right direction (conversion from tiles to local coordinates)
+
+	var head_offset = (dir_scaled * %SnakeManager.clock) / 16 # where is the head inside its tile, prevents jittering
+	var exact_head_pos =  %SnakeLayer.map_to_local(%SnakeManager.body[0]) + head_offset + dir_scaled * 3
+	
+	#position_tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
+	#position_tween.tween_property(self, "position", exact_head_pos, .05)
+	
+	zoom_tween = create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	zoom_tween.tween_property(self, "zoom", Vector2(2.5,2.5), .2)
+	zoom_tween.set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
+	zoom_tween.tween_property(self, "zoom", initial_zoom, 3.)
+	
 
 func random_offset() -> Vector2:
 	return Vector2(rng.randf_range(-curr_shake_strength, curr_shake_strength), rng.randf_range(-curr_shake_strength, curr_shake_strength))
@@ -25,16 +55,20 @@ func handle_shake(delta: float) -> void:
 		curr_shake_strength = lerpf(curr_shake_strength, 0, shake_fade * delta)
 		offset = random_offset()
 
+func any_tween_active():
+	return [position_tween, zoom_tween].any(func(t:Tween): return t and t.is_running())
+
 func _process(delta: float) -> void:
+	handle_shake(delta)
+	
+	if any_tween_active():
+		return
 	match curr_state:
 		STATE.START:
-			var snake_head_pos = %SnakeLayer.map_to_local(%SnakeManager.body[0])
-			var anchor = %SnakeLayer.map_to_local(%SnakeManager.body[0] + Direction.dir_to_vec(%SnakeManager.curr_dir)) # One tile after head
-			var dir_scaled = anchor - snake_head_pos # Vector of norm 1 in the right direction (conversion from tiles to local coordinates)
+			
+			position = lerp(position, get_target_pos(), .1)
 
-			var head_offset = (dir_scaled * %SnakeManager.clock) / 16 # where is the head inside its tile, prevents jittering
-			var exact_head_pos =  %SnakeLayer.map_to_local(%SnakeManager.body[0]) + head_offset
-			var diff_vector = exact_head_pos - starting_pos
-			position = lerp(position, starting_pos + diff_vector * max(0, diff_vector.length() - 10)* .001, .1)
 
-	handle_shake(delta)
+func _ready() -> void:
+	Signals.on_collision.connect(on_collision)
+	
